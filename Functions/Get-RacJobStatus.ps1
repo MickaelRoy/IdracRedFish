ac8c83bd8ad14660bf6bc8a3046fad97e1776801
Mickael ROY <mickael.roy.ext@boursorama.fr>
2024-04-10 17:11:20 +0200

    Fix jobid, add help

--- Functions/Get-RacJobStatus.ps1
+++ Functions/Get-RacJobStatus.ps1
@@ -1,4 +1,42 @@
 ﻿Function Get-RacJobStatus {
+<#
+    .SYNOPSIS
+    Gets the status of a job on an iDRAC (Integrated Dell Remote Access Controller) server.
+
+    .DESCRIPTION
+    The Get-RacJobStatus function queries a Dell server iDRAC to obtain the status of a specific job using various parameters to specify the iDRAC, authentication information, and other request options.
+
+    .PARAMETER Ip_Idrac
+    The IP address of the Dell iDRAC. Used with the `Hostname` parameter to resolve the IP address if only the hostname is provided.
+
+    .PARAMETER Hostname
+    The hostname of the Dell server. If specified, it is resolved to a corresponding IP address.
+
+    .PARAMETER Credential
+    The credentials required to access the iDRAC.
+
+    .PARAMETER Session
+    A custom session for web requests. This parameter can be supplied by another function named New-RacSession.
+
+    .PARAMETER JobId
+    The identifier of the job to retrieve the status for.
+
+    .PARAMETER NoProxy
+    Indicates whether the use of a proxy should be disabled for the web request.
+
+    .EXAMPLE
+    Get-RacJobStatus -Ip_Idrac "192.168.1.100" -Credential $Credential -JobId "123456789"
+    Gets the status of the job with the specified identifier on the iDRAC with the IP address 192.168.1.100 using the provided credentials.
+
+    .EXAMPLE
+    Get-RacJobStatus -Hostname "server01.example.com" -Credential $Credential -NoProxy
+    Gets the status of the job on the iDRAC of the server "server01.example.com" using the provided credentials and disabling the use of a proxy.
+
+    .NOTES
+    Author: Mickael ROY
+    Date created: 08-10-2023
+#>
+
     [CmdletBinding(DefaultParameterSetName = 'Host')]
     param(
         [Parameter(ParameterSetName = 'Ip', Mandatory = $true, Position = 0)]
@@ -19,6 +57,8 @@
         [Parameter(ParameterSetName = 'Session', Mandatory = $true, Position = 0)]
         [ValidateNotNullOrEmpty()]
         [PSCustomObject]$Session,
+
+        [String]$JobId,
 
         [Switch]$NoProxy
 	)
@@ -57,7 +97,11 @@
         If ($PSVersionTable.PSVersion.Major -gt 5) { $WebRequestParameter.SkipCertificateCheck = $true }
     }
 
-    $WebRequestParameter.uri = "https://$Ip_Idrac/redfish/v1/TaskService/Tasks/$JobId"
+    $Uri = "https://$Ip_Idrac/redfish/v1/TaskService/Tasks/"
+    
+    If ($JobId) { $Uri = [String]::Concat($Uri, $JobId) }
+
+    $WebRequestParameter.uri = $Uri
     $WebRequestParameter.Method = 'Get'
 
     $GetJobResult = Invoke-WebRequest @WebRequestParameter -ErrorAction Stop
